3742d1a0168ccbf9d2ecd324510e624f70004900
ODBreno <bre.oliveira.dias@gmail.com>
2024-04-15 17:35:33 -0300

    [WIP] Adição Funionalidades
    Tentativa inicial com as 3 funções funcionando

--- barramento.py
+++ barramento.py
@@ -6,30 +6,33 @@ app = Flask(__name__)
 @app.route('/servicos', methods=['POST'])
 def servicos():
     data = request.json
-    url_responde_mensagem = 'http://localhost:5002'
-    url_altera_arquivo = 'http://localhost:5003'
-    url_calculo = 'http://localhost:5004'
+    url_responde_mensagem = 'http://localhost:5002/mensagem'
+    url_altera_arquivo = 'http://localhost:5005/alterarArquivo'
+    url_calculo = 'http://localhost:5004/calcular'
+    print(data)
     if 'servico' in data:
         servico = data['servico']
         if servico == 'mensagem':
-            resposta = requests.get(url_responde_mensagem, json=data)
-            if resposta.status_code == 200:
-                mensagem = resposta.json().get('mensagem')
+            resposta = requests.get(url_responde_mensagem, json=data).json()
+            print(resposta)
+            if resposta:
+                mensagem = resposta.get('resposta')
                 return jsonify({'mensagem': mensagem})
         elif servico == 'arquivo':
-            resposta = requests.get(url_altera_arquivo, json=data)
-            if resposta.status_code == 200:
-                mensagem = resposta.json().get('mensagem')    
+            resposta = requests.get(url_altera_arquivo, json=data).json()
+            if resposta:
+                mensagem = resposta.get('mensagem')    
                 return jsonify({'mensagem': mensagem})
         elif servico == 'calculo':
-            resposta = requests.get(url_altera_arquivo, json=data)
-            if resposta.status_code == 200:
-                resultado = resposta.json().get('resultado')    
+            resposta = requests.get(url_calculo, json=data).json()
+            print(resposta)
+            if resposta:
+                resultado = resposta.get('resultado')    
                 return jsonify({'resultado': resultado})
         else:
-            return jsonify({'mensagem': 'Serviço não encontrado!'})
+            return jsonify({'resultado': 'Serviço não encontrado!'})
     else:
-        return jsonify({'error': 'Nenhum serviço enviado!'}), 400
+        return jsonify({'resultado': 'Nenhum serviço enviado!'}), 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)

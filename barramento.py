386af535142ee782a446d8cce747623a3d9d7da9
Breno Oliveira Dias <bre.oliveira.dias@gmail.com>
2024-04-15 13:38:07 -0300

    Update barramento.py

--- barramento.py
+++ barramento.py
@@ -1,25 +1,35 @@
 from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name__)
 
 @app.route('/servicos', methods=['POST'])
 def servicos():
     data = request.json
-    
+    url_responde_mensagem = 'http://localhost:5002'
+    url_altera_arquivo = 'http://localhost:5003'
+    url_calculo = 'http://localhost:5004'
     if 'servico' in data:
         servico = data['servico']
         if servico == 'mensagem':
-            mensagem = data['mensagem']
-            if mensagem:
-                return jsonify({'mensagem': f'Mensagem "{mensagem}" recebida com sucesso!'})
-            else:
-                return jsonify({'mensagem': 'Nenhuma mensagem recebida!'})
+            resposta = requests.get(url_responde_mensagem, json=data)
+            if resposta.status_code == 200:
+                mensagem = resposta.json().get('mensagem')
+                return jsonify({'mensagem': mensagem})
         elif servico == 'arquivo':
-            return jsonify({'mensagem': 'Arquivo alterado com sucesso!'})
+            resposta = requests.get(url_altera_arquivo, json=data)
+            if resposta.status_code == 200:
+                mensagem = resposta.json().get('mensagem')    
+                return jsonify({'mensagem': mensagem})
         elif servico == 'calculo':
-            return jsonify({'mensagem': 'Cálculo realizado com sucesso!'})
+            resposta = requests.get(url_altera_arquivo, json=data)
+            if resposta.status_code == 200:
+                resultado = resposta.json().get('resultado')    
+                return jsonify({'resultado': resultado})
+        else:
+            return jsonify({'mensagem': 'Serviço não encontrado!'})
     else:
-        return jsonify({'mensagem': 'Serviço não encontrado!'})
+        return jsonify({'error': 'Nenhum serviço enviado!'}), 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)

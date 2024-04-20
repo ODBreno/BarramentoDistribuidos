33b1b6663ec4c277693beed1c5f22938bd0fef0c
Breno Oliveira Dias <bre.oliveira.dias@gmail.com>
2024-04-19 21:14:47 -0300

    [ADD] Serviço Composto Compra Créditos

--- barramento.py
+++ barramento.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 def servicos():
     data = request.json
     url_responde_mensagem = 'http://localhost:5002/mensagem'
-    url_altera_arquivo = 'http://localhost:5005/alterarArquivo'
     url_calculo = 'http://localhost:5004/calcular'
+    url_altera_arquivo = 'http://localhost:5005/alterarArquivo'
+    url_le_arquivo = 'http://localhost:5006/lerArquivo'
     print(data)
     if 'servico' in data:
         servico = data['servico']
@@ -20,6 +21,7 @@ def servicos():
                 return jsonify({'mensagem': mensagem})
         elif servico == 'arquivo':
             resposta = requests.get(url_altera_arquivo, json=data).json()
+            print(resposta)
             if resposta:
                 mensagem = resposta.get('mensagem')    
                 return jsonify({'mensagem': mensagem})
@@ -29,6 +31,32 @@ def servicos():
             if resposta:
                 resultado = resposta.get('resultado')    
                 return jsonify({'resultado': resultado})
+        elif servico == 'adicionar':
+            # Serviço composto para adicionar créditos
+            # Leitura do saldo antigo
+            compra = data.get('creditos')
+            r1 = requests.get(url_le_arquivo, json=data).json()
+            saldo_antigo = r1.get('saldo')
+            print(r1)
+            
+            # Cálculo do novo saldo
+            json_calculo = {
+                'numero1': saldo_antigo, 
+                'numero2': compra,
+                'operador': '+'
+            }
+            r2 = requests.get(url_calculo, json=json_calculo).json()
+            saldo_novo = r2.get('resultado')
+            print(r2)
+            
+            # Registro do novo saldo e envio da resposta
+            json_arquivo = {
+                'nome_arquivo': data.get('cpf'), 
+                'texto': saldo_novo
+            }
+            r3 = requests.get(url_altera_arquivo, json=json_arquivo).json()
+            print(r3)
+            return jsonify({'saldo': saldo_novo})
         else:
             return jsonify({'resultado': 'Serviço não encontrado!'})
     else:
